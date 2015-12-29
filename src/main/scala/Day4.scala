140d1326829548db8d6fc0235e5be0ca41e59686
Jakub Kozłowski <kubukoz@gmail.com>
2015-12-29 05:16:52 +0100

    Day 4 "cache"

--- src/main/scala/Day4.scala
+++ src/main/scala/Day4.scala
@@ -8,7 +8,8 @@ object Day4 {
 
   def main(args: Array[String]) {
     val input = "iwrupvqb"
-    println(Stream.iterate(0)(_ + 1).find(key => matches(md5(input + key), 5)))
-    println(Stream.iterate(0)(_ + 1).find(key => matches(md5(input + key), 6)))
+    val fiveZeroes = Stream.iterate(0)(_ + 1).find(key => matches(md5(input + key), 5))
+    println(fiveZeroes)
+    println(Stream.iterate(fiveZeroes.getOrElse(0))(_ + 1).find(key => matches(md5(input + key), 6)))
   }
 }

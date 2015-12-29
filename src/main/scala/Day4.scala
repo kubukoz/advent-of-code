1d1f2c7abe6adff7104161d5573267833c28105c
Jakub Kozłowski <kubukoz@gmail.com>
2015-12-29 05:11:46 +0100

    Day 4 solution, done earlier

---
+++ src/main/scala/Day4.scala
@@ -0,0 +1,14 @@
+import java.security.MessageDigest
+
+object Day4 {
+  def md5(s: String) =
+    MessageDigest.getInstance("MD5").digest(s.getBytes).map("%02X".format(_)).mkString
+
+  def matches(s: String, zeroes: Int) = s.startsWith("0" * zeroes)
+
+  def main(args: Array[String]) {
+    val input = "iwrupvqb"
+    println(Stream.iterate(0)(_ + 1).find(key => matches(md5(input + key), 5)))
+    println(Stream.iterate(0)(_ + 1).find(key => matches(md5(input + key), 6)))
+  }
+}

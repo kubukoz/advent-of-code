d249f895621abe2918da89c890d4bba52c34783e
Jakub Kozłowski <kubukoz@gmail.com>
2016-12-03 08:34:50 +0100

    Add solution for Day 3

---
+++ src/main/scala/com/kubukoz/adventofcode2016/Day3.scala
@@ -0,0 +1,26 @@
+package com.kubukoz.adventofcode2016
+
+object Day3 {
+  private def toMatrix(s: List[String]) = s.map(_.split("""\s+""").filterNot(_.trim.isEmpty).map(_.toInt))
+
+  private def isTriangle(a: Int, b: Int, c: Int) = a + b > c && a + c > b && b + c > a
+
+  def countTriangles(s: List[String]): Int = toMatrix(s).count {
+    case Array(a, b, c) => isTriangle(a, b, c)
+  }
+
+  def countTrianglesVertical(s: List[String]): Int = {
+    toMatrix(s).grouped(3).map {
+      _.transpose.count {
+        case Seq(a, b, c) => isTriangle(a, b, c)
+      }
+    }.sum
+  }
+
+  def main(args: Array[String]): Unit = {
+    val input = io.Source.fromURL(getClass.getResource("/day3.txt")).getLines().toList
+
+    println(countTriangles(input))
+    println(countTrianglesVertical(input))
+  }
+}

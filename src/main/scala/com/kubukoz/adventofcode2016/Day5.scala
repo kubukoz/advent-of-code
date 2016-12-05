e33d673b4037ad87529370709f45a65bfa1d7945
Jakub Kozłowski <kubukoz@gmail.com>
2016-12-05 15:08:01 +0100

    Add some improvements and make part 1 of day 1 actually work!

--- src/main/scala/com/kubukoz/adventofcode2016/Day5.scala
+++ src/main/scala/com/kubukoz/adventofcode2016/Day5.scala
@@ -3,25 +3,27 @@ package com.kubukoz.adventofcode2016
 import scala.annotation.tailrec
 
 object Day5 {
-  private def md5(s: String) =
-    java.security.MessageDigest.getInstance("MD5").digest(s.getBytes).take(4).map("%02X".format(_)).mkString
+  private val md = java.security.MessageDigest.getInstance("MD5")
+  private val rounds = 8
 
-  private def findPassword(s: String, rounds: Int, fun: (Int, String) => (Int, Char)): String = {
+  private def md5(s: String) = {
+    md.digest(s.getBytes).take(4).map("%02X".format(_)).mkString
+  }
+
+  private def findPassword(s: String, validateMd5: String => Boolean, fun: (Int, String) => (Int, Char)): String = {
     @tailrec
     def go(mem: Map[Int, Char], lastNum: Int): Map[Int, Char] = {
       @tailrec
       def findNextNumber(current: Int): (Int, String) = {
         val result = md5(s + current)
-        if (result.startsWith("0" * 5) && result(5).asDigit < rounds) (current, result)
+        if (result.startsWith("0" * 5) && validateMd5(result)) (current, result)
         else findNextNumber(current + 1)
       }
 
-      val (nextNum, nextNumResult) = findNextNumber(lastNum)
-      val funResult = fun(nextNum, nextNumResult)
+      val (nextNum, nextNumMD5) = findNextNumber(lastNum)
+      val (index, char) = fun(mem.size, nextNumMD5)
 
-      val newMem =
-        if (mem.isDefinedAt(funResult._1)) mem
-        else mem + funResult
+      val newMem = if (mem.isDefinedAt(index)) mem else mem + (index -> char)
 
       if (newMem.size == rounds) newMem
       else go(newMem, nextNum + 1)
@@ -32,9 +34,11 @@ object Day5 {
     (0 until rounds).map(theMap).mkString
   }
 
-  def findEasyPassword(s: String): String = findPassword(s, 8, (i, s) => (i, s(5)))
+  def findEasyPassword(s: String): String = findPassword(s, _ => true, (i, s) => (i, s(5)))
 
-  def findHardPassword(s: String): String = findPassword(s, 8, (_, s) => (s(5).asDigit, s(6)))
+  def findHardPassword(s: String): String = {
+    findPassword(s, result => result(5).asDigit < rounds, (_, s) => (s(5).asDigit, s(6)))
+  }
 
   def main(args: Array[String]): Unit = {
     val input = "ffykfhsq"
--- src/test/scala/com/kubukoz/adventofcode2016/Day5Tests.scala
+++ src/test/scala/com/kubukoz/adventofcode2016/Day5Tests.scala
@@ -4,9 +4,9 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class Day5Tests extends FlatSpec with Matchers {
   "Finding the password" should "work" in {
-//    Day5.findEasyPassword("abc") shouldBe "18f47a30"
+//    Day5.findEasyPassword("abc") shouldBe "18F47A30"
   }
   it should "work in part 2 too" in {
-//    Day5.findHardPassword("abc") shouldBe "05ace8e3"
+//    Day5.findHardPassword("abc") shouldBe "05ACE8E3"
   }
 }

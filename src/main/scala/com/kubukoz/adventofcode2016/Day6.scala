dbbe7665991364fa5b799c6ce999d3ee2e260253
Jakub Kozłowski <kubukoz@gmail.com>
2016-12-06 14:30:44 +0100

    Fix to avoid sorting the whole list

--- src/main/scala/com/kubukoz/adventofcode2016/Day6.scala
+++ src/main/scala/com/kubukoz/adventofcode2016/Day6.scala
@@ -1,19 +1,21 @@
 package com.kubukoz.adventofcode2016
 
 object Day6 {
-  private def findPasswordByFrequency(input: List[String], selectFun: List[Char] => Char): String = {
+  private def findPasswordByFrequency(input: List[String], selectFun: List[(Char, Int)] => (Char, Int)): String = {
     val charsByColumn = input.map {
       _.map(List(_))
     }.reduce { (a, b) =>
       (a zip b).map { case (chars, chars2) => chars ::: chars2 }
     }
 
-    charsByColumn.map(chList => selectFun(chList.sortBy(ch => chList.count(_ == ch)))).mkString
+    val charsWithCounts = charsByColumn.map { list => list.map(char => (char, list.count(_ == char))) }
+
+    charsWithCounts.map(chList => selectFun(chList)._1).mkString
   }
 
-  def findPasswordByMostPopular(input: List[String]): String = findPasswordByFrequency(input, _.last)
+  def findPasswordByMostPopular(input: List[String]): String = findPasswordByFrequency(input, _.maxBy(_._2))
 
-  def findPasswordByLeastPopular(input: List[String]): String = findPasswordByFrequency(input, _.head)
+  def findPasswordByLeastPopular(input: List[String]): String = findPasswordByFrequency(input, _.minBy(_._2))
 
   def main(args: Array[String]): Unit = {
     val source = io.Source.fromURL(getClass.getResource("/day6.txt"))

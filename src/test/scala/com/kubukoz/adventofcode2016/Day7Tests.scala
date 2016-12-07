70b8b24b33fb3c16828419180ba4fecb2a67cd2d
Jakub Kozłowski <kubukoz@gmail.com>
2016-12-07 11:46:06 +0100

    Add solution for day 7

---
+++ src/test/scala/com/kubukoz/adventofcode2016/Day7Tests.scala
@@ -0,0 +1,62 @@
+package com.kubukoz.adventofcode2016
+
+import com.kubukoz.adventofcode2016.Day7._
+import org.scalatest.{FlatSpec, Matchers}
+
+class Day7Tests extends FlatSpec with Matchers {
+  "supportsTLS" should "work properly for case 1" in {
+    supportsTLS("abba[mnop]qrst") shouldBe true
+  }
+
+  it should "work for case 2" in {
+    supportsTLS("abcd[bddb]xyyx") shouldBe false
+  }
+
+  it should "work for case 3" in {
+    supportsTLS("aaaa[qwer]tyui") shouldBe false
+  }
+
+  it should "work for case 4" in {
+    supportsTLS("ioxxoj[asdfgh]zxcvbn") shouldBe true
+  }
+
+  it should "work for case 5" in {
+    supportsTLS("asdf[dupa]oxxo") shouldBe true
+  }
+
+  it should "work for case 6" in {
+    supportsTLS("asdf[dupa]oxxo[yolo]swag") shouldBe true
+  }
+
+  it should "work for case 7" in {
+    supportsTLS("asdf[ollo]oxxo[yolo]swag") shouldBe false
+  }
+
+  it should "count the input" in {
+    input.count(supportsTLS) shouldBe 118
+  }
+
+  "supportsSSL" should "work properly for case 1" in {
+    supportsSSL("aba[bab]xyz") shouldBe true
+  }
+
+  it should "work for case 2" in {
+    supportsSSL("xyx[xyx]xyx") shouldBe false
+  }
+
+  it should "work for case 3" in {
+    supportsSSL("aaa[kek]eke") shouldBe true
+  }
+
+  it should "work for case 4" in {
+    supportsSSL("zazbz[bzb]cdb") shouldBe true
+  }
+
+  it should "work for my case" in {
+    supportsSSL("asdf[babXaba]dupa") shouldBe false
+  }
+
+  it should "count the input" in {
+    input.count(supportsSSL) shouldBe 260
+  }
+}

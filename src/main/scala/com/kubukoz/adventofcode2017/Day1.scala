17693a60d55782bbe90dbbf44d76e08e04eaf7a5
Jakub Kozłowski <kubukoz@gmail.com>
2017-12-01 16:07:33 +0100

    Add solutions from day 1

--- src/main/scala/com/kubukoz/adventofcode2017/Day1.scala
+++ src/main/scala/com/kubukoz/adventofcode2017/Day1.scala
@@ -1,7 +1,27 @@
 package com.kubukoz.adventofcode2017
 
 object Day1 {
-  def main(args: Array[String]): Unit = {
+  private def sumDist(dist: Int, s: Int*): Int = {
+    val megaList = s ++ s
 
+    s.zipWithIndex.map {
+      case (digit, i) => List(digit, megaList(i + dist))
+    }.filter(_.distinct.size == 1).map(_.head).sum
+  }
+
+  def sum1(s: Int*): Int = sumDist(1, s:_*)
+
+  def sumHalfway(s: Int*): Int = sumDist(s.length / 2, s:_*)
+
+  def inputSums(): (Int, Int) = {
+    val input = fileLines("/day1.txt")
+
+    val parsed: List[Int] = input.flatMap(_.map(_.toString.toInt))
+
+    (sum1(parsed: _*), sumHalfway(parsed:_*))
+  }
+
+  def main(args: Array[String]): Unit = {
+    println("Part 1, 2: " + inputSums())
   }
 }
--- src/test/scala/com/kubukoz/adventofcode2016/Day1Tests.scala
+++ src/test/scala/com/kubukoz/adventofcode2016/Day1Tests.scala
@@ -5,10 +5,22 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class Day1Tests extends FlatSpec with Matchers {
   "The first part" should "work for the given cases" in {
-
+    sum1(1, 1, 2, 2) shouldBe 3
+    sum1(1, 1, 1, 1) shouldBe 4
+    sum1(1, 2, 3, 4) shouldBe 0
+    sum1(9, 1, 2, 1, 2, 1, 2, 9) shouldBe 9
+    sum1(4, 9, 4, 7, 5, 1, 1, 3, 6, 8, 4) shouldBe 5
   }
 
-  "The second part" should "work for the given case" in {
+  "The second part" should "work for the given cases" in {
+    sumHalfway(1, 2, 1, 2) shouldBe 6
+    sumHalfway(1, 2, 2, 1) shouldBe 0
+    sumHalfway(1, 2, 3, 4, 2, 5) shouldBe 4
+    sumHalfway(1, 2, 3, 1, 2, 3) shouldBe 12
+    sumHalfway(1, 2, 1, 3, 1, 4, 1, 5) shouldBe 4
+  }
 
+  "Inputs" should "be properly handled" in {
+    inputSums() shouldBe (1150, 1064)
   }
 }

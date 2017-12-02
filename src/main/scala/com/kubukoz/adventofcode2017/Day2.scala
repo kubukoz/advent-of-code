98b551afb9aad545e854f72e9094d10a75a600bb
Jakub Kozłowski <kubukoz@gmail.com>
2017-12-02 18:15:37 +0100

    Improve solution for day 2

--- src/main/scala/com/kubukoz/adventofcode2017/Day2.scala
+++ src/main/scala/com/kubukoz/adventofcode2017/Day2.scala
@@ -12,28 +12,25 @@ object Day2 {
 
   val parseLine: String => List[Int] = _.split("""\s+""").map(_.toInt).toList
 
-  def diffMaxMin[T: Order : Numeric](line: List[T]): Option[T] = {
+  private def diffMaxMin[T: Order : Numeric](line: List[T]): Option[T] = {
     for {
       max <- line.maximumOption
       min <- line.minimumOption
     } yield max - min
   }
 
-  def divWhole[T: Numeric : Integral](line: List[T]): Option[T] = {
-    for {
-      a <- line.view
-      b <- line.view
-      if b != a
-      if b % a == 0
-    } yield b / a
-  }.headOption
+  private def divWhole[T: Integral](line: List[T]): Option[T] = {
+    line.combinations(2).map(_.sorted).collectFirst {
+      case a :: b :: Nil if b % a == 0 => b / a
+    }
+  }
 
-  def solve[LT, T: Numeric](input: List[LT])(f: LT => Option[T]): T =
+  private def solve[LT, T: Numeric](input: List[LT])(f: LT => Option[T]): T =
     input.flatMap(f(_)).sum
 
   def part1[T: Order : Numeric](input: List[List[T]]): T = solve(input)(diffMaxMin(_))
 
-  def part2[T: Numeric : Integral](input: List[List[T]]): T = solve(input)(divWhole(_))
+  def part2[T: Integral](input: List[List[T]]): T = solve(input)(divWhole(_))
 
   def main(args: Array[String]): Unit = {
     val input = fileLines("/day2.txt").map(parseLine)

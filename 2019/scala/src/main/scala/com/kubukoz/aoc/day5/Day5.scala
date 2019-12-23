c67de77edd202d12a3d8ce7b7312a7a69d473f1a
Jakub Kozłowski <kubukoz@gmail.com>
2019-12-23 03:25:34 +0100

    Add input, reference type, some cleanup and dereferencing

--- 2019/scala/src/main/scala/com/kubukoz/aoc/day5/Day5.scala
+++ 2019/scala/src/main/scala/com/kubukoz/aoc/day5/Day5.scala
@@ -20,7 +20,7 @@ import cats.effect.concurrent.Ref
 private[day5] object data {
 
   @Lenses
-  final case class Program(tokens: List[Token], nextPosition: Option[Position])
+  final case class Program(tokens: List[Token], nextPosition: Option[Position], stack: List[Token])
 
   final case class Position(position: Int)
 
@@ -33,14 +33,15 @@ private[day5] object data {
 
   sealed trait Instruction extends Product with Serializable
 
+  //noinspection ZeroIndexToHead
   object Instruction {
     case object Halt extends Instruction
 
-    final case class Combine(from: NonEmptyList[Position], to: Position, combine: Combine.Way)
+    final case class Combine(from: NonEmptyList[Reference], to: Position, combine: Combine.Way)
         extends Instruction
 
-    final case class Save(at: Position)   extends Instruction
-    final case class Load(from: Position) extends Instruction
+    final case class Save(to: Position)    extends Instruction
+    final case class Load(from: Reference) extends Instruction
 
     object Combine {
       sealed trait Way extends Product with Serializable
@@ -52,30 +53,72 @@ private[day5] object data {
     }
 
     val decode: PartialFunction[List[Token], Instruction] = {
-      case Token.Halt :: _                         => Halt
-      case Token.Add :: from1 :: from2 :: to :: _  => combine(from1, from2, to, Combine.Way.Add)
-      case Token.Mult :: from1 :: from2 :: to :: _ => combine(from1, from2, to, Combine.Way.Mult)
-      case Token.Save :: at :: _                   => Save(toPosition(at))
-      case Token.Load :: from :: _                 => Load(toPosition(from))
+      case Token.Halt :: _ => Halt
+      case Token.Add(modes) :: from1 :: from2 :: to :: _ =>
+        combine(modes, from1, from2, to, Combine.Way.Add)
+      case Token.Mult(modes) :: from1 :: from2 :: to :: _ =>
+        combine(modes, from1, from2, to, Combine.Way.Mult)
+      case Token.Save :: at :: _          => Save(toPosition(at))
+      case Token.Load(modes) :: from :: _ => Load(from.withMode(modes.values(0)))
     }
 
-    private val toPosition: Token => Position = t => Position(t.token)
+    private def combine(modes: Modes, from1: Token, from2: Token, to: Token, merge: Combine.Way) =
+      Instruction.Combine(
+        NonEmptyList.of(
+          from1.withMode(modes.values(0)),
+          from2.withMode(modes.values(1))
+        ),
+        toPosition(to),
+        merge
+      )
+  }
+  val toPosition: Token => Position = t => Position(t.token)
 
-    private def combine(from1: Token, from2: Token, to: Token, merge: Combine.Way) =
-      Instruction.Combine(NonEmptyList.of(from1, from2).map(toPosition), toPosition(to), merge)
+  sealed trait Reference extends Product with Serializable
+
+  object Reference {
+    final case class ByPosition(pos: Position) extends Reference
+    final case class ByValue(value: Int)       extends Reference
   }
 
   final case class Token(token: Int) {
+
+    val withMode: Mode => Reference = {
+      case Mode.Positional => Reference.ByPosition(Position(token))
+      case Mode.Immediate  => Reference.ByValue(token)
+    }
+
     def add(another: Token): Token  = Token(token + another.token)
     def mult(another: Token): Token = Token(token * another.token)
   }
 
+  sealed trait Mode extends Product with Serializable
+
+  object Mode {
+    case object Positional extends Mode
+    case object Immediate  extends Mode
+  }
+
+  final case class Modes(values: List[Mode])
+
   object Token {
     val Halt = Token(99)
-    val Add  = Token(1)
-    val Mult = Token(2)
+
+    object Add {
+      //opcode 01 / 1
+      def unapply(tok: Token): Option[Modes] = ???
+    }
+
+    object Mult {
+      //opcode 02 / 2
+      def unapply(tok: Token): Option[Modes] = ???
+    }
     val Save = Token(3)
-    val Load = Token(4)
+
+    object Load {
+      //opcode 04 / 4
+      def unapply(tok: Token): Option[Modes] = ???
+    }
 
     val parse: String => Token = s => Token(s.trim.toInt)
   }
@@ -103,7 +146,7 @@ object Interpreter {
     val running: F[Boolean] = State.inspect(_.nextPosition.nonEmpty)
 
     val nextOp: F[Instruction] =
-      State.inspect(s => Instruction.decode(s.tokens.drop(s.nextPosition.fold(0)(_.position))))
+      State.inspect(s => Instruction.decode(s.tokens.drop(s.nextPosition.foldMap(_.position))))
 
     def atPosition(pos: Position): F[Token] =
       State.inspect(
@@ -116,26 +159,39 @@ object Interpreter {
     def setAtPosition(pos: Position, token: Token): F[Unit] =
       State.modify(Program.tokenAt(pos.position).set(token))
 
+    val dereference: Reference => F[Token] = {
+      case Reference.ByValue(v)      => Token(v).pure[F]
+      case Reference.ByPosition(pos) => atPosition(pos)
+    }
+
+    def push(token: Token): F[Unit] = State.modify(Program.stack.modify(token :: _))
+
+    val pop: F[Token] = State.inspect(Program.stack.get(_)).map(_.head) <*
+      State.modify(Program.stack.modify(_.tail))
+
+    def jumpTo(position: Option[Position]): F[Unit] =
+      State.modify(Program.nextPosition.set(position))
+
+    def nextPosition(steps: Int) = State.inspect { prog =>
+      prog.nextPosition.map(_.position + steps).filter(_ < prog.tokens.length).map(Position)
+    }
+
+    def jumpBy(steps: Int): F[Unit] = nextPosition(steps).flatMap(jumpTo)
+
     val runOp: Instruction => F[Unit] = {
       case Instruction.Halt => State.modify(Program.nextPosition.set(None))
+      case Instruction.Load(from) =>
+        dereference(from).flatMap(push) *> jumpBy(2)
       case Instruction.Combine(from, to, combine) =>
         val reduce: (Token, Token) => Token = combine match {
           case Way.Add  => _ add _
           case Way.Mult => _ mult _
         }
 
-        val nextPosition = State.inspect { prog =>
-          prog.nextPosition.map(_.position + 4).filter(_ < prog.tokens.length).map(Position)
-        }
-
-        def jumpTo(position: Option[Position]): F[Unit] =
-          State.modify(Program.nextPosition.set(position))
-
         for {
-          newValue <- from.nonEmptyTraverse(atPosition).map(_.reduceLeft(reduce))
+          newValue <- from.nonEmptyTraverse(dereference).map(_.reduceLeft(reduce))
           _        <- setAtPosition(to, newValue)
-          next     <- nextPosition
-          _        <- jumpTo(next)
+          _        <- jumpBy(4)
         } yield ()
     }
 
@@ -169,7 +225,7 @@ object Day5 extends IOApp {
 
   def parse(input: String): Program = {
     val tokens = input.split(",").toList.map(Token.parse)
-    Program(tokens, Position(0).some)
+    Program(tokens, Position(0).some, Nil)
   }
 
   def part1(input: Program): IO[Int] =

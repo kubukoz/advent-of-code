5632217e438ab26a69065d2679065f9a12b153e7
Jakub Kozłowski <kubukoz@gmail.com>
2020-11-27 01:23:17 +0100

    Types

--- 2020/scala/zio/src/main/scala/com/kubukoz/aoc/day0/Day0ZIO.scala
+++ 2020/scala/zio/src/main/scala/com/kubukoz/aoc/day0/Day0ZIO.scala
@@ -2,13 +2,12 @@ package com.kubukoz.aoc.day0
 
 import com.kubukoz.aoc.ZUtil
 import com.kubukoz.aoc.ZUtilApp
-import zio.Has
 import zio.ZIO
 import zio.console._
 
 object Day0ZIO extends ZUtilApp {
 
-  def runProg: ZIO[Has[ZUtil.Service] with zio.ZEnv, Any, Any] =
+  def runProg: ZIO[ZUtil with zio.ZEnv, Any, Any] =
     ZUtil
       .readFile("files/day0.txt")
       .flatMap(putStrLn(_))

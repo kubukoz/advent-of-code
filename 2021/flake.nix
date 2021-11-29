08bc51449a39cbe5b33662c3bbc1d7c43df15608
Jakub Kozłowski <kubukoz@gmail.com>
2021-11-29 15:40:03 +0100

    Init 2021

---
+++ 2021/flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.unison.url = "github:ceedubs/unison-nix";
+
+  outputs = { nixpkgs, flake-utils, unison, ... }: flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (
+    system:
+      let
+        pkgs = import nixpkgs { inherit system; overlays = [ unison.overlay ]; };
+      in
+        {
+          devShell = pkgs.mkShell { buildInputs = [ pkgs.unison-ucm ]; };
+        }
+  );
+}

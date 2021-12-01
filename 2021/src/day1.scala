d12d305c496fe27f81f6f438652ca704e61aec0e
Jakub Kozłowski <kubukoz@gmail.com>
2021-12-01 19:10:26 +0100

    Day 1

--- 2021/src/day1.scala
+++ 2021/src/day1.scala
@@ -1,5 +1,10 @@
-object day1 extends App {
-  import lib._
+object Day1 extends App {
 
-  println(readAllLines("day1.txt"))
+  val input = lib.readAllLines("day1.txt")
+  val values = input.filterNot(_.trim.isEmpty).map(_.toInt)
+
+  def result(data: List[Int]) = data.zip(data.tail).count { case (before, after) => after > before }
+
+  println("Part 1: " + result(values))
+  println("Part 2: " + result(values.sliding(3).map(_.sum).toList))
 }

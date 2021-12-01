a2735cf29edd3e3a1d0c8a73fcc2420a910d016e
Jakub Kozłowski <kubukoz@gmail.com>
2021-12-01 04:14:11 +0100

    Add stdlib

--- 2021/conf.scala
+++ 2021/conf.scala
@@ -1,4 +1,4 @@
-// using scala 3.1.0
+// using scala 2.13.7
 // using plugin org.polyvariant:::better-tostring:0.3.11
-// using option -Ykind-projector
+// using plugin org.typelevel:::kind-projector:0.13.2
 // using lib org.typelevel::cats-effect:3.3.0
---
+++ 2021/src/lib.scala
@@ -0,0 +1,14 @@
+import java.nio.file.Files
+
+import java.nio.file.Paths
+import scala.io.Source
+import scala.util.Using
+
+object utils {
+
+  def readAllLines(fileName: String): List[String] =
+    Using(Source.fromFile(s"./resources/$fileName"))(_.getLines.toList).get
+
+  def readAll(fileName: String): String = readAllLines(fileName).mkString("\n")
+
+}

d20e3d813c229575f8ece9ee3c28d3e6f3cab9a9
Jakub Kozłowski <kubukoz@gmail.com>
2021-12-01 18:32:19 +0100

    aarch64 support

--- 2021/flake.nix
+++ 2021/flake.nix
@@ -3,10 +3,19 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.unison.url = "github:ceedubs/unison-nix";
 
-  outputs = { nixpkgs, flake-utils, unison, ... }: flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (
+  outputs = { nixpkgs, flake-utils, unison, ... }: flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ] (
     system:
     let
-      pkgs = import nixpkgs { inherit system; overlays = [ unison.overlay ]; };
+      x86_pkgs = import nixpkgs { localSystem = "x86_64-darwin"; overlays = [ unison.overlay ]; };
+
+      aarch64-overrides = final: prev:
+        if prev.stdenv.isAarch64 then {
+          inherit (x86_pkgs) unison-ucm scala-cli;
+        } else { };
+
+
+      pkgs = import nixpkgs { inherit system; overlays = [ unison.overlay aarch64-overrides ]; };
+
     in
     {
       devShell = pkgs.mkShell { buildInputs = [ pkgs.unison-ucm pkgs.scala-cli ]; };

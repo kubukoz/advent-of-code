a4669f809e5adc0251c6602c9e62615bd821a8db
Jakub Kozłowski <kubukoz@gmail.com>
2021-12-11 02:06:27 +0100

    Day6: simplify

--- 2021/src/main/scala/aoc/day6.scala
+++ 2021/src/main/scala/aoc/day6.scala
@@ -1,73 +1,29 @@
 package aoc
 
 import aoc.lib._
-import cats.Monad
-import cats.data.OptionT
-import cats.data.State
 import cats.implicits._
-import cats.mtl.Stateful
-
-import util.chaining._
 
 object Day6 extends App {
-
-  final case class CacheMap(cache: Map[(Int, Int), Long], hits: Long, misses: Long) {
-    def +(kv: (((Int, Int), Long))): CacheMap = copy(cache = cache + kv)
-
-    def get(k: (Int, Int)): Option[Long] = cache.get(k)
-  }
-
-  def go[F[_]: Monad](
-    fish: Int,
-    rounds: Int,
-  )(
-    implicit S: Stateful[F, CacheMap]
-  ): F[Long] = {
-    def store(result: Long) = S.modify(_ + ((fish, rounds) -> result))
-
-    def updateStats(result: Option[_]): F[Unit] =
-      result
-        .fold(
-          S.modify(s => s.copy(misses = s.misses + 1))
-        )(_ => S.modify(s => s.copy(hits = s.hits + 1)))
-
-    S.get.flatMap {
-      _.get((fish, rounds))
-        .pure[F]
-        .flatTap(updateStats(_))
-        .pipe(OptionT(_))
-        .getOrElseF {
-          if (rounds == 0)
-            1L.pure[F] /* no more rounds, count this one fish */
-          else {
-            if (fish == 0) {
-              (
-                go[F](6, rounds - 1),
-                go[F](8, rounds - 1),
-              ).mapN(_ + _)
-            } else
-              go[F](fish - 1, rounds - 1)
-          }.flatTap(store)
-        }
-    }
-  }
 
   def solve(input: List[String]): (Long, Long) = {
     val parsed = input.mkString.split(",").map(_.toInt).toList
 
     def goAll(rounds: Int): Long = {
-      val (s, result) =
-        parsed
-          .foldMapM(go[State[CacheMap, *]](_, rounds))
-          .run(CacheMap(Map.empty, 0, 0))
-          .value
+      def recurse(generation: Map[Int, Long], rounds: Int): Long =
+        if (rounds == 0)
+          generation.values.sum
+        else {
+          val newGeneration = generation.toList.foldMap {
+            case (0, amount)    => Map(6 -> amount, 8 -> amount)
+            case (fish, amount) => Map(fish - 1 -> amount)
+          }
 
-      println(
-        s"Cache hits: ${s.hits}, misses: ${s.misses}, hits/total rate: ${(s.hits * 100) / (s.hits + s.misses)}%"
-      )
+          recurse(newGeneration, rounds - 1)
+        }
 
-      result
+      recurse(parsed.groupBy(identity).map(_.map(_.size.toLong)), rounds)
     }
+
     (80, 256).bimap(goAll, goAll)
   }
 

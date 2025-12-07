6e74c89d991407ae9fc1aad852efde55a2ce4af2
Jakub Kozłowski <kubukoz@gmail.com>
2025-12-08 00:49:05 +0100

    day 7 part 1

---
+++ 2025/day7/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"aoc2025/shared"
+	"fmt"
+	"slices"
+	"strings"
+)
+
+func main() {
+	input := shared.ReadFile("sample.txt")
+	input = shared.ReadFile("input.txt")
+	lines := strings.Split(input, "\n")
+
+	fmt.Printf("Part 1: %v\n", part1(lines))
+}
+
+func part1(lines []string) (total int) {
+	// assume the start is in the first line
+	startIndex := strings.IndexByte(lines[0], 'S')
+
+	return branches(lines, 0, startIndex)
+}
+
+func branches(lines []string, currentLineIndex int, startIndex int) int {
+	currentByteIndices := []int{startIndex}
+	splitCount := 0
+
+	for {
+		currentLineIndex++
+		if currentLineIndex == len(lines) {
+			break
+		}
+		line := lines[currentLineIndex]
+
+		newCharIndices := []int{}
+
+		for _, pointer := range currentByteIndices {
+			atPointer := line[pointer]
+			switch atPointer {
+			case '.':
+				newCharIndices = appendIfMissing(newCharIndices, pointer)
+			case '^':
+				// if pointer > 0 {
+				newCharIndices = appendIfMissing(newCharIndices, pointer-1)
+				// }
+				// if pointer < len(line) {
+				newCharIndices = appendIfMissing(newCharIndices, pointer+1)
+				// }
+				splitCount++
+			}
+		}
+		currentByteIndices = newCharIndices
+	}
+
+	return splitCount
+}
+
+// not very efficient but what can you do
+func appendIfMissing[T comparable](slice []T, item T) []T {
+	if slices.Contains(slice, item) {
+		return slice
+	} else {
+		return append(slice, item)
+	}
+}

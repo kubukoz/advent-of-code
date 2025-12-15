ea318d6d645786f01cf34ee6f5511c454750915c
Jakub Kozłowski <kubukoz@gmail.com>
2025-12-15 22:50:37 +0100

    bootstrap for day 12

---
+++ 2025/day12/main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	// input := shared.ReadFile("sample.txt")
+	// input = shared.ReadFile("input.txt")
+
+	// data := parse(input)
+
+	// fmt.Printf("Part 1: %v\n", part1(data))
+
+}
